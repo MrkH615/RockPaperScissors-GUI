320ec9e49b2b38adf50643bfa6584e9b1dc1f095
Mark Hajnal <mhajnal@sympatico.ca>
2020-01-03 12:46:39 -0500

    Deleted console.logs and added function to replay game

--- main.js
+++ main.js
@@ -20,33 +20,23 @@ function writeMessage(result){
 }  
 
 function writeComputerChoice() {
-  computerChoice.textContent = `The computer chooses ${computerSelection}`;
+  computerChoice.textContent = `The computer chooses ${computerSelection}.  `;
 }
 
 function nameWinner(playerPoints, computerPoints) {
 
- 
-  
-
    if (playerPoints > computerPoints) {
-      winner.textContent = "Game over! You win! Reload this page to play again.  ";
+      winner.textContent = "Game over! You win!  Click anywhere on page to play again.  ";
    } else if (computerPoints > playerPoints) {
-    winner.textContent = "Game over!  You lose!  Reload this page to play again.  ";
+    winner.textContent = "Game over!  You lose!  Click anywhere on page to play again.  ";
    } else {
-    winner.textContent = "Game over!  It's a tie!  Reload this page to play again.  ";
+    winner.textContent = "Game over!  It's a tie! Click anywhere on page to play again.  ";
    }
-   
-   
+
+   document.addEventListener('click', event => {
+     window.location.reload();
+   });
 }
-
-
-
-
-
-
-
-
-
 
  function computerPlay() {  
   let min;
@@ -57,7 +47,6 @@ function nameWinner(playerPoints, computerPoints) {
      return Math.floor(Math.random() * (max - min + 1)) + min;
    }
    
-
    if (randomInteger(min, max) === 1) {
      computerSelection = "rock";
    } else if (randomInteger(min, max) === 2) {
@@ -66,23 +55,16 @@ function nameWinner(playerPoints, computerPoints) {
       computerSelection = "scissors";
    }
  
-
  return computerSelection;
- 
+
 }
  
-
-
-
 function playGame() {
-
-  
 
   let rock = document.querySelector('#rock');
     rock.addEventListener('click', playerChooses = () => {
       playerSelection = 'rock';
       computerPlay();
-      console.log(playerSelection);
       return  playerSelection;
     } );
         
@@ -90,7 +72,6 @@ let paper = document.querySelector('#paper');
   paper.addEventListener('click', playerChooses = () => {
     playerSelection = 'paper';
     computerPlay();
-    console.log(playerSelection);
     return  playerSelection;
   });
 
@@ -98,30 +79,23 @@ let scissors= document.querySelector('#scissors');
 scissors.addEventListener('click', playerChooses = () => {
     playerSelection = 'scissors';
     computerPlay();
-    console.log(playerSelection);
     return  playerSelection;
 });
 
-
-
   function playerWins(playerSelection, computerSelection) {
-    result = `Computer loses!  ${playerSelection} beats ${computerSelection}`;
+    result = `Computer loses round ${roundNumber}!  ${playerSelection} beats ${computerSelection}`;
     playerPoints++;
-    console.log(result);
     writeComputerChoice();
     writeMessage(`${result}`);
-    console.log(`Player ${playerPoints}`);
     makeHumanScore(playerPoints);
     return result;
   } 
 
   function computerWins(playerSelection, computerSelection) {
-      result = `You lose! ${computerSelection} beats ${playerSelection}`;
+      result = `You lose round ${roundNumber}! ${computerSelection} beats ${playerSelection}`;
      computerPoints++;
-     console.log(result);
      writeComputerChoice();
      writeMessage(`${result}`);
-     console.log(`Computer ${computerPoints}`);
      makeComputerScore(computerPoints);
       return result;
   } 
@@ -130,7 +104,6 @@ scissors.addEventListener('click', playerChooses = () => {
      result = `It's a tie!`;
      writeComputerChoice();
     writeMessage(`${result}`);
-    console.log(result);
      return result;
   }  
 let roundNumber = 0;
@@ -142,80 +115,43 @@ let roundNumber = 0;
 
     if (playerSelection === computerSelection) { 
       roundNumber++;
-      console.log(`round ${roundNumber}`);
-      console.log(`computerSelection ${computerSelection}`);
       result = tie(playerSelection, computerSelection);
      
-
     }  else  if (playerSelection ==="rock" && computerSelection === "paper") {
       roundNumber++;
-      console.log(`round ${roundNumber}`);
-      console.log(`computerSelection ${computerSelection}`);
       result = computerWins(playerSelection, computerSelection);
      
-
     }  else if (playerSelection === "rock"  && computerSelection === "scissors") {
       roundNumber++;
-      console.log(`round ${roundNumber}`);
-      console.log(`computerSelection ${computerSelection}`);
       result = playerWins(playerSelection, computerSelection);
      
-
-
     } else if (playerSelection === "paper"  && computerSelection === "scissors") {
       roundNumber++;
-      console.log(`round ${roundNumber}`);
-      console.log(`computerSelection ${computerSelection}`);
       result = computerWins(playerSelection, computerSelection);
       
-
     }   else if (playerSelection === "paper" && computerSelection === "rock") {
       roundNumber++;
-      console.log(`round ${roundNumber}`);
-      console.log(`computerSelection ${computerSelection}`);
       result = playerWins(playerSelection, computerSelection);
       
-
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
       roundNumber++;
-      console.log(`round ${roundNumber}`);
-      console.log(`computerSelection ${computerSelection}`);
       result = playerWins(playerSelection, computerSelection); 
     
-
     }  else if (playerSelection === "scissors" && computerSelection === "rock") {
       roundNumber++;
-      console.log(`round ${roundNumber}`);
-      console.log(`computerSelection ${computerSelection}`);
       result = computerWins(playerSelection, computerSelection);
-     // console.log(result); 
-    //return result;
     }  
   } else {
-     
-    
-    console.log(`total player points:  ${playerPoints}`);
-     console.log(`total computer points:  ${computerPoints}`);
-
-    if (playerPoints > computerPoints) {
-          console.log("You win!");
-    } else if (computerPoints > playerPoints) {
-          console.log ("You lose!");
-    } else {
-          console.log ("It's a tie!");
-    }
-    console.log("Game over");
-
+   
     nameWinner(playerPoints, computerPoints);
   }
-}
- });
+ }
+});
 }
 
 function makeHumanScore(playerPoints) {
   const humanPoints = document.querySelector('#human');
   humanPoints.textContent = `Human:  ${playerPoints}`;
-
 } 
 
 function makeComputerScore(computerPoints) {
@@ -223,31 +159,5 @@ function makeComputerScore(computerPoints) {
   cmptrPoints.textContent=`Computer:  ${computerPoints}`;
 } 
 
-
-
- 
     playGame();
   
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-   
-
-        
-
